18501fe629797b68f2d96dd1ed683826465e648e
Guillaume LEDUC <gduc.37@gmail.com>
2022-09-21 17:38:27 +0200

    Adding Logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,13 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import ThemeToggle from './themeToggle'
+import Nav from './Nav'
+
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className='text-slate-900 dark:text-slate-50 bg-slate-50 dark:bg-slate-900'>
-      <nav className='fixed mt-2 flex items-center'>
-        <ul className='list-none text-lg flex justify-evenly'>
-          <li className=''><Link to="/" className="layout-link">Home</Link></li>
-          <li className=''><Link to="/about" className="layout-link">About</Link></li>
-        </ul>
-        <ThemeToggle />
-      </nav>
+        <Nav />
       <main className='p-0 m-0'>
-        <section className='grid grid-rows-4 place-items-center h-screen'>
+        <section className='grid grid-rows-4 place-items-center min-h-screen'>
             <div className=' row-start-2 row-span-3'>
                 <h1 className='text-7xl font-bold'>{pageTitle}</h1>
                 {children}
