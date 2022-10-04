3c18dd5e3beea6f9bedf410cb40952402ff89337
Guillaume LEDUC <gduc.37@gmail.com>
2022-10-04 22:37:25 +0200

    Resolution d'affichage

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,20 +16,23 @@ const Layout = ({ pageTitle, children }) => {
     <div className='min-h-screen'>
         <Nav />
         <div className='p-0 m-0'>
-                <div className='relative top-24'>
-                    <h1 className='text-6xl font-bold w-full text-center underline decoration-2 underline-offset-8'>{pageTitle}</h1>
-                </div>
-                <div className=''>
-                    {children}
-                </div>
-                <div className='w-1/2 mx-auto relative bottom-0 border-t dark:border-zinc-50/10 border-zinc-900/10'>
-                    <div  className='flex justify-around items-center py-2'> 
-                        <p>@ElDucche's Portfolio.</p>
-                        <a href='https://github.com/ElDucche'><AiFillGithub aria-label='GitHub' size={30}/></a>
-                        <a href='https://twitter.com/ElDucche'><AiOutlineTwitter aria-label='Twitter' size={30}/></a>
-                        <a href='https://www.linkedin.com/in/guillaume-leduc-731a10b4/'><AiFillLinkedin aria-label='Linkedin'size={30}/></a>
+            <p className='invisible'>.</p>
+            <div className='p-0 mt-24 '>
+                    <div className=''>
+                        <h1 className='text-6xl font-bold w-full text-center underline decoration-2 underline-offset-8'>{pageTitle}</h1>
                     </div>
-                </div>
+                    <div className=''>
+                        {children}
+                    </div>
+                    <div className='md:w-1/2 w-full mx-auto row-start-3 border-t dark:border-zinc-50/10 border-zinc-900/10'>
+                        <div  className='flex justify-around items-center py-2'> 
+                            <p className='font-extralight'>ElDucche's Portfolio.</p>
+                            <a href='https://github.com/ElDucche'><AiFillGithub aria-label='GitHub' size={30}/></a>
+                            <a href='https://twitter.com/ElDucche'><AiOutlineTwitter aria-label='Twitter' size={30}/></a>
+                            <a href='https://www.linkedin.com/in/guillaume-leduc-731a10b4/'><AiFillLinkedin aria-label='Linkedin'size={30}/></a>
+                        </div>
+                    </div>
+            </div>
         </div>
     </div>
   )
