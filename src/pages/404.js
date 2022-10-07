94cb63f1d2a4adefe66c1a8baafe4cfdfbda1b23
Guillaume LEDUC <gduc.37@gmail.com>
2022-10-07 11:05:36 +0200

    Ajout du quatrième article

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const NotFoundPage = () => {
   return (
-    <div className="grid place-items-center content-center">
-      <h1 className="text-5xl">Page not found</h1>
-      <p className="bg-slate-500 texte-slate-50">
-        Sorry <span aria-label="emoji sad">😔</span>, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </div>
+    <Layout pageTitle="404">
+      <div className="grid place-items-center content-center min-h-screen">
+        <h1 className="text-5xl scrollHidden">Page not found</h1>
+        <p className="bg-slate-500 text-slate-50 my-4 scrollHidden p-3 rounded">
+          Désolé, cette page n'existe pas encore. Peut-être n'existera-t-elle jamais...
+          <br />
+          {process.env.NODE_ENV === "development" ? (
+            <>
+              <br />
+              Try creating a page in <code>src/pages/</code>.
+              <br />
+            </>
+          ) : null}
+          <br />
+          <Link to="/">Home</Link>.
+        </p>
+      </div>
+    </Layout>
   )
 }
 
